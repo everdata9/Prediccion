8810b7521e95bb34eccfafdd6f007d1ba5062838
alfarove@aresep.go.cr <alfarove@aresep.go.cr>
2025-02-02 00:26:51 -0600

    Cambios

--- algoritmoOne.py
+++ algoritmoOne.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 from datetime import datetime
+import matplotlib.dates as mdates
 from streamlit_option_menu import option_menu
 import locale
 import io  # Para la descarga del gráfico
 
 # Configurar la localización en español para los meses
-#locale.setlocale(locale.LC_TIME, "es_ES.UTF-8")
+# locale.setlocale(locale.LC_TIME, "es_ES.UTF-8")
 
 # Variable de fecha de actualización
 FECHA_ACTUALIZACION = "2025-01-31"
@@ -16,6 +16,7 @@ FECHA_ACTUALIZACION = "2025-01-31"
 # Rutas de los archivos Excel para uso global
 EXCEL_FILE_PATH = "20241221 Cronograma IT v3 - copia (1).xlsx"
 EXCEL_RECURSO_PATH = "Estimacion_RecursosIT.xlsx"
+
 
 def cargar_datos():
     try:
@@ -32,26 +33,30 @@ def cargar_datos():
 
 def cargar_recursos():
     try:
-        df_recursos = pd.read_excel(EXCEL_RECURSO_PATH, engine="openpyxl")
+        df_recursos = pd.read_excel(EXCEL_RECURSO_PATH, engine="openpyxl").fillna(0)
+        df_recursos.columns = ['Funcionario', 'Mes', 'Anno', 'Horas']
         return df_recursos
     except Exception as e:
         st.error(f"Error al cargar el archivo de recursos: {e}")
         return None
 
+
 def extraer_anios(df):
-    columnas_fecha = [col for col in df.columns if isinstance(col, datetime)]
-    anios = sorted(set(col.year for col in columnas_fecha))
+    anios = sorted(df['Anno'].unique())
     return anios
 
+
 def extraer_meses(df, anio_seleccionado):
-    columnas_fecha = [col for col in df.columns if isinstance(col, datetime) and col.year == anio_seleccionado]
-    meses = sorted(set(col.strftime('%B') for col in columnas_fecha), key=lambda x: datetime.strptime(x, '%B').month)
-    meses_espanol = [datetime.strptime(mes, '%B').strftime('%B').capitalize() for mes in meses]
-    if 'Enero' not in meses_espanol:
-        meses_espanol.insert(0, 'Enero')
-    if 'Febrero' not in meses_espanol:
-        meses_espanol.insert(1, 'Febrero')
-    return meses_espanol
+    meses_ordenados = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    meses = sorted(df[df['Anno'] == anio_seleccionado]['Mes'].unique(), key=lambda x: meses_ordenados.index(x))
+    return meses
+
+
+def ordenar_meses(df):
+    meses_ordenados = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    df['Mes'] = pd.Categorical(df['Mes'], categories=meses_ordenados, ordered=True)
+    return df.sort_values('Mes')
+
 
 def mostrar_informacion_proyecto():
     st.title("📄 Información del Proyecto")
@@ -64,6 +69,7 @@ def mostrar_informacion_proyecto():
     st.write("- Interfaz interactiva con Streamlit.")
     st.write(f"🔄 Última actualización: {FECHA_ACTUALIZACION}")
 
+
 def mostrar_recurso_humano(selected_nombre, selected_year, selected_month):
     st.title("👥 Recurso Humano del Proyecto")
     st.write(f"🔄 Última actualización: {FECHA_ACTUALIZACION}")
@@ -71,18 +77,70 @@ def mostrar_recurso_humano(selected_nombre, selected_year, selected_month):
     df_recursos = cargar_recursos()
     if df_recursos is not None:
         if selected_year != 'Todos':
-            columnas_fecha = [col for col in df_recursos.columns if isinstance(col, datetime) and col.year == selected_year]
-            if selected_month != 'Todos':
-                columnas_fecha = [col for col in columnas_fecha if col.strftime('%B').capitalize() == selected_month]
-            df_recursos = df_recursos[['Nombre'] + columnas_fecha]
+            df_recursos = df_recursos[df_recursos['Anno'] == selected_year]
+        if selected_month != 'Todos':
+            df_recursos = df_recursos[df_recursos['Mes'] == selected_month]
 
+        df_grouped_ano = df_recursos.groupby(['Funcionario', 'Anno'])['Horas'].sum().reset_index()
         if selected_nombre != 'Todos':
-            st.write(f"### Información del recurso seleccionado: {selected_nombre}")
-            recurso_info = df_recursos[df_recursos['Nombre'] == selected_nombre]
-            st.dataframe(recurso_info)
-        else:
-            st.write("### Lista completa de recursos humanos")
-            st.dataframe(df_recursos)
+            df_grouped_ano = df_grouped_ano[df_grouped_ano['Funcionario'] == selected_nombre]
+
+        fig, ax = plt.subplots(figsize=(12, 6))
+        bottom = pd.Series([0] * len(df_grouped_ano['Anno'].unique()), index=df_grouped_ano['Anno'].unique())
+        for nombre, group in df_grouped_ano.groupby('Funcionario'):
+            bars = ax.bar(group['Anno'].astype(str), group['Horas'], label=nombre, bottom=bottom[group['Anno']].values)
+            bottom[group['Anno']] += group['Horas'].values
+
+            for bar in bars:
+                height = bar.get_height()
+                if height > 0:  # Solo anotar si las horas son mayores que cero
+                    bar_center = bar.get_y() + height / 2
+                    ax.annotate(f'{int(height)}', 
+                                xy=(bar.get_x() + bar.get_width() / 2, bar_center),
+                                xytext=(0, 0), 
+                                textcoords="offset points",
+                                ha='center', 
+                                va='center', 
+                                fontsize=10, 
+                                color='white')
+
+        ax.set_title('Sumatoria de Horas por Año', fontsize=16, fontweight='bold')
+        ax.set_xlabel('Año', fontsize=14)
+        ax.set_ylabel('Horas', fontsize=14)
+        ax.legend(title='Recurso')
+        st.pyplot(fig)
+
+        st.write("### Sumatoria de horas por mes")
+        df_grouped_mes = df_recursos.groupby(['Funcionario', 'Mes'])['Horas'].sum().reset_index()
+        df_grouped_mes = ordenar_meses(df_grouped_mes)
+
+        fig, ax = plt.subplots(figsize=(12, 6))
+        bottom = pd.Series([0] * len(df_grouped_mes['Mes'].unique()), index=df_grouped_mes['Mes'].unique())
+        for nombre, group in df_grouped_mes.groupby('Funcionario'):
+            bars = ax.bar(group['Mes'], group['Horas'], label=nombre, bottom=bottom[group['Mes']].values)
+            bottom[group['Mes']] += group['Horas'].values
+
+            for bar in bars:
+                height = bar.get_height()
+                if height > 0:  # Solo anotar si las horas son mayores que cero
+                    bar_center = bar.get_y() + height / 2
+                    ax.annotate(f'{int(height)}', 
+                                xy=(bar.get_x() + bar.get_width() / 2, bar_center),
+                                xytext=(0, 0), 
+                                textcoords="offset points",
+                                ha='center', 
+                                va='center', 
+                                fontsize=10, 
+                                color='white')
+        if selected_year != 'Todos' or (selected_year == 'Todos' and selected_nombre == 'Todos'):
+            ax.set_title('Sumatoria de Horas por Mes', fontsize=16, fontweight='bold')
+            ax.set_xlabel('Mes', fontsize=14)
+            ax.set_ylabel('Horas', fontsize=14)
+            ax.legend(title='Recurso')
+            plt.xticks(rotation=45)
+            st.pyplot(fig)
+
+
 
 def mostrar_grafico():
     st.title("🗕️ Visualización de la Línea del Tiempo")
@@ -168,6 +226,7 @@ def mostrar_grafico():
             mime="image/png"
         )
 
+
 def main():
     with st.sidebar:
         opcion = option_menu(
@@ -178,25 +237,22 @@ def main():
             default_index=0
         )
 
-        selected_nombre = None
+        selected_nombre = 'Todos'
         selected_year = 'Todos'
         selected_month = 'Todos'
 
         if opcion == "Recurso Humano":
             df_recursos = cargar_recursos()
             if df_recursos is not None:
-                nombres = df_recursos['Nombre'].unique().tolist()
+                nombres = df_recursos['Funcionario'].unique().tolist()
                 nombres.insert(0, 'Todos')
-                selected_nombre = st.selectbox("Seleccione un recurso humano", nombres)
+                selected_nombre = st.selectbox("Seleccione un recurso humano", nombres, index=0)
 
                 anios = extraer_anios(df_recursos)
                 anios.insert(0, 'Todos')
                 selected_year = st.selectbox("Seleccione un año del proyecto", anios)
 
-                if selected_year != 'Todos':
-                    meses = extraer_meses(df_recursos, selected_year)
-                    meses.insert(0, 'Todos')
-                    selected_month = st.selectbox("Seleccione un mes del proyecto", meses)
+
 
     if opcion == "Información del Proyecto":
         mostrar_informacion_proyecto()
@@ -204,6 +260,7 @@ def main():
         mostrar_grafico()
     elif opcion == "Recurso Humano":
         mostrar_recurso_humano(selected_nombre, selected_year, selected_month)
+
 
 if __name__ == "__main__":
     main()
--- venv/respaldo.py
+++ venv/respaldo.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 from datetime import datetime
 from streamlit_option_menu import option_menu
 import locale
@@ -17,6 +16,7 @@ FECHA_ACTUALIZACION = "2025-01-31"
 EXCEL_FILE_PATH = "20241221 Cronograma IT v3 - copia (1).xlsx"
 EXCEL_RECURSO_PATH = "Estimacion_RecursosIT.xlsx"
 
+
 def cargar_datos():
     try:
         df = pd.read_excel(EXCEL_FILE_PATH, engine="openpyxl")
@@ -30,18 +30,33 @@ def cargar_datos():
         st.error(f"Error al cargar el archivo Excel: {e}")
         return None
 
+
 def cargar_recursos():
     try:
-        df_recursos = pd.read_excel(EXCEL_RECURSO_PATH, engine="openpyxl")
+        df_recursos = pd.read_excel(EXCEL_RECURSO_PATH, engine="openpyxl").fillna(0)
+        df_recursos.columns = ['Funcionario', 'Mes', 'Anno', 'Horas']
         return df_recursos
     except Exception as e:
         st.error(f"Error al cargar el archivo de recursos: {e}")
         return None
 
+
 def extraer_anios(df):
-    columnas_fecha = [col for col in df.columns if isinstance(col, datetime)]
-    anios = sorted(set(col.year for col in columnas_fecha))
+    anios = sorted(df['Anno'].unique())
     return anios
+
+
+def extraer_meses(df, anio_seleccionado):
+    meses_ordenados = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    meses = sorted(df[df['Anno'] == anio_seleccionado]['Mes'].unique(), key=lambda x: meses_ordenados.index(x))
+    return meses
+
+
+def ordenar_meses(df):
+    meses_ordenados = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    df['Mes'] = pd.Categorical(df['Mes'], categories=meses_ordenados, ordered=True)
+    return df.sort_values('Mes')
+
 
 def mostrar_informacion_proyecto():
     st.title("📄 Información del Proyecto")
@@ -54,107 +69,74 @@ def mostrar_informacion_proyecto():
     st.write("- Interfaz interactiva con Streamlit.")
     st.write(f"🔄 Última actualización: {FECHA_ACTUALIZACION}")
 
-def mostrar_recurso_humano(selected_nombre, selected_year):
+
+def mostrar_recurso_humano(selected_nombre, selected_year, selected_month):
     st.title("👥 Recurso Humano del Proyecto")
     st.write(f"🔄 Última actualización: {FECHA_ACTUALIZACION}")
 
     df_recursos = cargar_recursos()
     if df_recursos is not None:
         if selected_year != 'Todos':
-            columnas_fecha = [col for col in df_recursos.columns if isinstance(col, datetime) and col.year == selected_year]
-            df_recursos = df_recursos[['Nombre'] + columnas_fecha]
+            df_recursos = df_recursos[df_recursos['Anno'] == selected_year]
+        if selected_month != 'Todos':
+            df_recursos = df_recursos[df_recursos['Mes'] == selected_month]
 
+        df_grouped_ano = df_recursos.groupby(['Funcionario', 'Anno'])['Horas'].sum().reset_index()
         if selected_nombre != 'Todos':
-            st.write(f"### Información del recurso seleccionado: {selected_nombre}")
-            recurso_info = df_recursos[df_recursos['Nombre'] == selected_nombre]
-            st.dataframe(recurso_info)
-        else:
-            st.write("### Lista completa de recursos humanos")
-            st.dataframe(df_recursos)
+            df_grouped_ano = df_grouped_ano[df_grouped_ano['Funcionario'] == selected_nombre]
+
+        fig, ax = plt.subplots(figsize=(12, 6))
+        bottom = pd.Series([0] * len(df_grouped_ano['Anno'].unique()), index=df_grouped_ano['Anno'].unique())
+        for nombre, group in df_grouped_ano.groupby('Funcionario'):
+            bars = ax.bar(group['Anno'].astype(str), group['Horas'], label=nombre, bottom=bottom[group['Anno']].values)
+            bottom[group['Anno']] += group['Horas'].values
+
+            for bar in bars:
+                height = bar.get_height()
+                bar_center = bar.get_y() + height / 2
+                ax.annotate(f'{int(height)}', xy=(bar.get_x() + bar.get_width() / 2, bar_center),
+                            xytext=(0, 0), textcoords="offset points",
+                            ha='center', va='center', fontsize=10, color='white')
+
+        ax.set_title('Sumatoria de Horas por Año', fontsize=16, fontweight='bold')
+        ax.set_xlabel('Año', fontsize=14)
+        ax.set_ylabel('Horas', fontsize=14)
+        ax.legend(title='Recurso')
+        st.pyplot(fig)
+
+        st.write("### Sumatoria de horas por mes")
+        df_grouped_mes = df_recursos.groupby(['Funcionario', 'Mes'])['Horas'].sum().reset_index()
+        df_grouped_mes = ordenar_meses(df_grouped_mes)
+
+        fig, ax = plt.subplots(figsize=(12, 6))
+        bottom = pd.Series([0] * len(df_grouped_mes['Mes'].unique()), index=df_grouped_mes['Mes'].unique())
+        for nombre, group in df_grouped_mes.groupby('Funcionario'):
+            bars = ax.bar(group['Mes'], group['Horas'], label=nombre, bottom=bottom[group['Mes']].values)
+            bottom[group['Mes']] += group['Horas'].values
+
+            for bar in bars:
+                height = bar.get_height()
+                bar_center = bar.get_y() + height / 2
+                ax.annotate(f'{int(height)}', xy=(bar.get_x() + bar.get_width() / 2, bar_center),
+                            xytext=(0, 0), textcoords="offset points",
+                            ha='center', va='center', fontsize=10, color='white')
+
+        ax.set_title('Sumatoria de Horas por Mes', fontsize=16, fontweight='bold')
+        ax.set_xlabel('Mes', fontsize=14)
+        ax.set_ylabel('Horas', fontsize=14)
+        ax.legend(title='Recurso')
+        plt.xticks(rotation=45)
+        st.pyplot(fig)
+
 
 def mostrar_grafico():
-    st.title("🗕️ Visualización de la Línea del Tiempo")
+    st.title("�헏️ Visualización de la Línea del Tiempo")
     st.write(f"🔄 Última actualización: {FECHA_ACTUALIZACION}")
 
     df = cargar_datos()
     if df is not None:
-        years = sorted(set(df['Year Start'].dropna().astype(int)).union(set(df['Year Finish'].dropna().astype(int))))
-        if 2025 not in years:
-            years.append(2025)
-        years = ['Todos'] + sorted(set(years))
-        selected_year = st.sidebar.selectbox("🗕️ Seleccione un año", years, index=0)
+        st.dataframe(df)
 
-        df_filtered = df[df['Outline Level'] == 1].copy()
-
-        if selected_year != 'Todos':
-            df_filtered = df_filtered[(df_filtered['Year Start'] == selected_year) | (df_filtered['Year Finish'] == selected_year)]
-
-        df_filtered = df_filtered.sort_values(by='Start', ascending=True)
-
-        selected_task = None
-        if selected_year != 'Todos':
-            task_names = ['Todas'] + df_filtered['Name'].unique().tolist()
-            selected_task = st.sidebar.selectbox("📋 Seleccione una tarea", task_names, index=0)
-            if selected_task != "Todas":
-                task_outline_level_1 = df_filtered[df_filtered['Name'] == selected_task]
-                if not task_outline_level_1.empty:
-                    task_start = task_outline_level_1.iloc[0]['Start']
-                    task_finish = task_outline_level_1.iloc[0]['Finish']
-
-                    df_filtered = df[
-                        (df['Outline Level'].isin([1, 2])) &
-                        (df['Start'] >= task_start) &
-                        (df['Finish'] <= task_finish)
-                    ]
-
-        st.write("### Línea del tiempo de los registros con Outline Level 1 y 2")
-        fig, ax = plt.subplots(figsize=(18, 10))
-        ax.set_facecolor('white')
-        df_filtered = df_filtered.reset_index(drop=True)
-        y_positions = range(len(df_filtered) - 1, -1, -1)
-
-        bars = ax.barh(y_positions, df_filtered['Finish'] - df_filtered['Start'], left=df_filtered['Start'], color='green', height=0.6)
-
-        for bar, (_, row) in zip(bars, df_filtered.iterrows()):
-            start_text = row['Start'].strftime('%d/%m/%y') if pd.notnull(row['Start']) else ""
-            finish_text = row['Finish'].strftime('%d/%m/%y') if pd.notnull(row['Finish']) else ""
-            bar_center = bar.get_y() + bar.get_height() / 2
-
-            ax.text(row['Start'], bar_center, start_text, verticalalignment='center', horizontalalignment='right', fontsize=11, color='black')
-            ax.text(row['Finish'], bar_center, finish_text, verticalalignment='center', horizontalalignment='left', fontsize=11, color='black')
-            ax.text(row['Start'] + (row['Finish'] - row['Start']) / 2, bar_center + 0.3, row['Name'], verticalalignment='bottom', horizontalalignment='center', fontsize=11, fontweight='bold', color='black')
-
-        ax.set_xlabel("Fecha", fontsize=14, fontweight='bold')
-        ax.set_ylabel("", fontsize=14, fontweight='bold')
-        ax.set_title("Línea del tiempo de tareas nivel 1 y 2", fontsize=16, fontweight='bold')
-        ax.set_yticks(y_positions)
-        ax.set_yticklabels([])
-
-        if selected_year == 'Todos':
-            ax.xaxis.set_major_locator(mdates.YearLocator())
-            ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
-            ax.set_xlim(left=pd.Timestamp(year=2025, month=1, day=1))
-        else:
-            ax.xaxis.set_major_locator(mdates.MonthLocator())
-            ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
-
-            today = datetime.today()
-            if today.year == selected_year:
-                ax.axvline(today, color='red', linestyle='--', linewidth=2)
-
-        ax.grid(False)
-        plt.xticks(fontsize=14, fontweight='bold', color='black', rotation=0)
-
-        st.pyplot(fig)
-        buffer = io.BytesIO()
-        plt.savefig(buffer, format="png", bbox_inches="tight")
-        buffer.seek(0)
-        st.download_button(
-            label="👅 Descargar gráfico",
-            data=buffer,
-            file_name="grafico_linea_tiempo.png",
-            mime="image/png"
-        )
 
 def main():
     with st.sidebar:
@@ -166,26 +148,33 @@ def main():
             default_index=0
         )
 
-        selected_nombre = None
+        selected_nombre = 'Todos'
         selected_year = 'Todos'
+        selected_month = 'Todos'
 
         if opcion == "Recurso Humano":
             df_recursos = cargar_recursos()
             if df_recursos is not None:
-                nombres = df_recursos['Nombre'].unique().tolist()
+                nombres = df_recursos['Funcionario'].unique().tolist()
                 nombres.insert(0, 'Todos')
-                selected_nombre = st.selectbox("Seleccione un recurso humano", nombres)
+                selected_nombre = st.selectbox("Seleccione un recurso humano", nombres, index=0)
 
                 anios = extraer_anios(df_recursos)
                 anios.insert(0, 'Todos')
                 selected_year = st.selectbox("Seleccione un año del proyecto", anios)
+
+                if selected_year != 'Todos':
+                    meses = extraer_meses(df_recursos, selected_year)
+                    meses.insert(0, 'Todos')
+                    selected_month = st.selectbox("Seleccione un mes del proyecto", meses)
 
     if opcion == "Información del Proyecto":
         mostrar_informacion_proyecto()
     elif opcion == "Visualización de la Línea del Tiempo":
         mostrar_grafico()
     elif opcion == "Recurso Humano":
-        mostrar_recurso_humano(selected_nombre, selected_year)
+        mostrar_recurso_humano(selected_nombre, selected_year, selected_month)
+
 
 if __name__ == "__main__":
     main()
